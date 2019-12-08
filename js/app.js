6d99b3c4325fb693a65ca5a4320a3531606f4422
yousef2213 <mandooyousef988@gmail.com>
2019-12-08 03:43:28 +0200

    Finish Project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
-//$(function(){
-//    $("#red").toggle(function(){
-//        $("#red").class("fas fa-star");
-//    })
-//})
+var img1 = document.getElementById("img-1");
+var img2 = document.getElementById("img-2");
+var img3 = document.getElementById("img-3");
+var img4 = document.getElementById("img-4");
+var img5 = document.getElementById("img-5");
+var img6 = document.getElementById("img-6");
+var img7 = document.getElementById("img-7");
+
+img2.onclick = function(){
+    img1.src = "img/img-products/product-2.png";
+}
+img3.onclick = function(){
+    img1.src = "img/img-products/product-3.png";
+}
+img4.onclick = function(){
+    img1.src = "img/img-products/product-4.png";
+}
+img5.onclick = function(){
+    img1.src = "img/img-products/product-5.png";
+}
+img6.onclick = function(){
+    img1.src = "img/img-products/product-6.png";
+}
+img7.onclick = function(){
+    img1.src = "img/img-products/product-7.png";
+}
